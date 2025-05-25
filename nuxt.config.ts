20ad21d6157c7c1c039d04788de388b4e8ef7662
Luan Henrique Souza Dantas <luan.dantas@solucx.com.br>
2025-05-25 14:45:18 -0300

    chore: remove server-side rendering (SSR) configuration from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   modules: ['@nuxt/content'],
